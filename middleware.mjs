923d10eaa9570dfd009d9502cdfe2fb396554a5f
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2017-11-24 17:23:42 -0800

    Guard against missing config.

--- middleware.mjs
+++ middleware.mjs
@@ -1,6 +1,7 @@
 import serveStatic from 'serve-static';
 
 export default function karmaStatic (config) {
+  config.static = config.static || {};
   var path = config.static.path;
   if (!path) {
     return function (req, res, next) {
